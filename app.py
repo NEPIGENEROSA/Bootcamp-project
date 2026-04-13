8737102c876e99116e44f3a56cce84c17292c4c6
NEPIGENEROSA <nepiloutogenerosa@gmail.com>
2026-04-13 22:11:50 +0200

    ESTRUCTURA FINAL CORREGIDA

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__) # El objeto debe llamarse 'app' exactamente.
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# CRÍTICO: Sin espacios dentro de las comillas
 if __name__ == "__main__":
     app.run()
