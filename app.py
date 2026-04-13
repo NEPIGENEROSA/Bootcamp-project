a7813852f07a59946abc749ecc8aa7ef1324c2c2
NEPIGENEROSA <nepiloutogenerosa@gmail.com>
2026-04-13 20:31:21 +0200

    Corrigiendo dependencias y estructura

--- app.py
+++ app.py
@@ -2,11 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal: sirve el archivo index.html de la carpeta /templates
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    # El modo debug permite que la web se actualice sola cuando cambies el código
-    app.run(debug=True)
+# No pongas nada más por ahora, Vercel solo necesita el objeto "app"
