28513cea2c74b2cc5b74440eafb1b6f77d4b05e3
NEPIGENEROSA <nepiloutogenerosa@gmail.com>
2026-04-21 19:50:42 +0200

    Uso de path.join para compatibilidad Vercel

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const config = require('./server/src/config/env'); 
-const taskRoutes = require('./server/src/routes/taskRoutes');
+const express = require('express');
+const cors = require('cors');
+const path = require('path'); // Añade esta línea
+
+// Usamos path.join para que funcione tanto en Windows como en Vercel
+const config = require(path.join(__dirname, 'server/src/config/env'));
+const taskRoutes = require(path.join(__dirname, 'server/src/routes/taskRoutes'));
+
+const app = express();
+// ... (el resto del código se queda igual)
